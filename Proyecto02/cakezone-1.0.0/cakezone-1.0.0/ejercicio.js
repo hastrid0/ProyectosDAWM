791221aa6f20b4ec18175c43e7457163fe3e3ee5
hastrid0 <proyectos-hst@outlook.com>
2022-08-07 20:18:17 -0500

    rutas con datos quemados

--- Proyecto02/cakezone-1.0.0/cakezone-1.0.0/ejercicio.js
+++ Proyecto02/cakezone-1.0.0/cakezone-1.0.0/ejercicio.js
@@ -1,24 +1,29 @@
 const marvel = {
     render: () => {
-      const urlAPI = 'https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=e2fc4684487f89859c98bfe8d08eebca&hash=b10e7770bba8ddbc01dace2060449360';
-      const container = document.querySelector('#marvel-row');
+      const urlAPI = 'https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=e2fc4684487f"89859c98bfe8d08eebca&hash=b10e7770bba8ddbc01dace2060449360';
+      const container = document.getElementsByClassName("container-fluid.py-5")
       let contentHTML = '';
-       // console.log('wtf')
+      console.log('wtf')
       fetch(urlAPI)
         .then(res => res.json())
         .then((json) => {
           console.log(json)
           for (const hero of json.data.results) {
             let urlHero = hero.urls[0].url;
-            contentHTML += `
-              <div class="col-md-4">
-                  <a href="${urlHero}" target="_blank">
-                    <img src="${hero.thumbnail.path}.${hero.thumbnail.extension}" alt="${hero.name}" class="img-thumbnail">
-                  </a>
-                  <h3 class="title">${hero.name}</h3>
-              </div>`;
+            var elemnt= document.createElement("col")
+           //contentHTML += `
+          
+             // <div class="col-md-4">
+               //   <a href="${urlHero}" target="_blank">
+                 //   <img src="${hero.thumbnail.path}.${hero.thumbnail.extension}" alt="${hero.name}" class="img-thumbnail">
+                 // </a>
+                  //<h3 class="title">${hero.name}</h3>
+              //</div>`;
           }
-          container.innerHTML = contentHTML;
+          elemnt.innerHTML("<div class= \"card shadow-sm\"><img class=\"bd-placeholder-img card-img-top\" width=\"100%\" height=\"225\" xmlns=\"http://www.w3.org/2000/svg\" role=\"img\" aria-label=\"Placeholder: Thumbnail\" preserveAspectRatio=\"xMidYMid slice\" focusable=\"false\"><title>Placeholder</title><rect width=\"100%\" height=\"100%\" fill=\"#55595c\"/><text x=\"50%\" y=\"50%\" fill=\"#eceeef\" dy=\".3em\">Thumbnail</text></img\>div class=\"card-body\"\><p class=\"card-text\">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p\><div class=\"d-flex justify-content-between align-items-center\">\)")
+          var lin= document.getElementsByClassName("col")
+          document.
+          //container.innerHTML = contentHTML;
         })
     }
   };
